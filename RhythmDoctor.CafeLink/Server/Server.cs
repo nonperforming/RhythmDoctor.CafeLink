04d80a57cc8739a98ce1bd4a5020d0eb32e3b847
Non <43197300+nonperforming@users.noreply.github.com>
2026-08-16 21:07:10 +1200

    Server: Allow `Accept: */*`

--- RhythmDoctor.CafeLink/Server/Server.cs
+++ RhythmDoctor.CafeLink/Server/Server.cs
@@ -71,12 +71,19 @@ internal class Server
 
     if (!httpRequest.IsLocal && !Plugin.Configuration.AllowRemoteRequests.Value)
     {
+      Plugin.Logger.LogWarning($"[{nameof(Server)}] Blocked request from remote {httpRequest.RemoteEndPoint}");
       httpResponse.StatusCode = (int)HttpStatusCode.Forbidden;
       goto SendResponseAndGetReadyForNextRequest;
     }
 
-    if (httpRequest.AcceptTypes?.Contains("application/json") != true)
+    if (
+      httpRequest.AcceptTypes is null
+      || !(httpRequest.AcceptTypes.Contains("*/*") || httpRequest.AcceptTypes.Contains("application/json"))
+    )
     {
+      Plugin.Logger.LogWarning(
+        $"[{nameof(Server)}] Unacceptable Accept types: {string.Join(",", httpRequest.AcceptTypes ?? ["(null)"])}"
+      );
       httpResponse.StatusCode = (int)HttpStatusCode.NotAcceptable;
       goto SendResponseAndGetReadyForNextRequest;
     }
@@ -94,6 +101,7 @@ internal class Server
     IResponse response;
     if (string.IsNullOrEmpty(endpoint))
     {
+      Plugin.Logger.LogWarning($"[{nameof(Server)}] No endpoint specified in request.");
       response = new ErrorResponse(HttpStatusCode.BadRequest, "No endpoint specified.");
       goto SendResponseWithDataAndGetReadyForNextRequest;
     }
@@ -108,6 +116,7 @@ internal class Server
     }
     else
     {
+      Plugin.Logger.LogWarning($"[{nameof(Server)}] Unknown endpoint '{endpoint}'.");
       response = new ErrorResponse(HttpStatusCode.NotFound, "Unknown endpoint.");
     }
 
